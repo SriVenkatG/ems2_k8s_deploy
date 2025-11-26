7b83c5024b8d08dcd92607774594726fd8684c98
SriVenkatG <2300031666@kluniversity.in>
2025-11-26 23:43:48 +0530

    Update SecurityConfig.java

--- ems-backend/src/main/java/com/example/ems/config/SecurityConfig.java
+++ ems-backend/src/main/java/com/example/ems/config/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration cfg = new CorsConfiguration();
-        cfg.setAllowedOrigins(List.of("http://localhost:5173"));
+        cfg.setAllowedOrigins(List.of("http://localhost:5173","http://localhost:30310"));
         cfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         cfg.setAllowedHeaders(List.of("*"));
         cfg.setAllowCredentials(true);
